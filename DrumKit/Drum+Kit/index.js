2ac2c0b008f181fad212011302c6116aa549fbbb
Sneha <snehaparasu2006@gmail.com>
2024-08-11 15:14:26 +0300

    v1

---
+++ DrumKit/Drum+Kit/index.js
@@ -0,0 +1,65 @@
+//find the number of drums
+var len=document.querySelectorAll(".drum").length;       
+
+//to play sound
+for( var i=0; i<len; ++i){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){ 
+        makeSound(this.innerHTML);
+        addAnimation(this.innerHTML);
+});
+
+//to press key
+    document.querySelectorAll(".drum")[i].addEventListener("keydown",function(event){
+        makeSound(event.key);
+        addAnimation(event.key);
+    });
+}
+
+//to add sound to the buttons when clicked
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var kickbass =new Audio("sounds/kick-bass.mp3");
+            kickbass.play();
+            break;
+        case "l":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+//to add animation to the buttons when clicked
+function addAnimation(currentKey) {
+    document.querySelector("." + currentKey).classList.add("pressed");
+
+    setTimeout(function() {
+        document.querySelector("." + currentKey).classList.remove("pressed");
+    
+    }, 100);
+
+}
+
